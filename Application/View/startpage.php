f2b2052586b1f7e72f239c36f069ed1c22b6ba7c
Sinan SALIH <snnslh@outlook.com>
2019-09-17 23:48:34 +0300

    Update media

--- Application/View/startpage.php
+++ Application/View/startpage.php
@@ -7,13 +7,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Ness PHP | Application Under Maintenance</title>
+	<title>Ness PHP | New Application</title>
 
     <style type="text/css">
     body{
         margin: 0px;
         padding: 0px; 
-        background-color: #09102C;
+        background-color: #16192A;
     }
     .header-box{
         padding-top: 90px;
@@ -25,7 +25,7 @@
     .header-box h1{
         text-align: center;
         vertical-align: middle;
-        color:#41CD52;
+        color:#EE4540;
         font-weight: lighter;
         text-transform: uppercase;
         font-size: 60px;
@@ -61,7 +61,7 @@
     .header-box a:hover{
         text-decoration: none;
         font-family: 'Segoe UI';
-        border-bottom: 2px solid #41CD52;
+        border-bottom: 2px solid #EE4540;
     }
     .footer-box{
         position: absolute;
@@ -73,7 +73,7 @@
     }
     .footer-box h1{
         font-size: 37px;
-        color:#0F1A46;
+        color:#510A32;
         font-family: 'Segoe UI';
         font-weight: lighter;
     }
@@ -90,7 +90,7 @@
 <body>
 
 <div class="header-box">
-    <img src="<?= Ness\ContentManager::getImage('ness_logo_rounded.png');?>" width="120"/>
+    <img src="<?= Ness\ContentManager::getImage('ness_logo.png');?>" width="220"/>
     <h1 class="disable-selection0"><?= Ness\Configuration::getTitle();?></h1>
     <h5 class="disable-selection0"><?= Ness\Configuration::getDescription();?></h5>
 
